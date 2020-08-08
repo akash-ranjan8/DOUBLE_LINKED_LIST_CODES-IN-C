983e9c5868687d3808009d61439d924e23eafddf
akash-ranjan8 <56363385+akash-ranjan8@users.noreply.github.com>
2020-08-09 02:00:55 +0530

    Add files via upload

---
+++ double_linked_list_creation.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{ int data;
+  struct node *next;
+  struct node *prev;
+};
+struct node *head=NULL;
+void create(int d)
+{ struct node *new;
+  new=(struct node*)malloc(sizeof(struct node));
+  new->data=d;
+  new->next=NULL;
+  new->prev=NULL;
+  if(head==NULL)
+  { head=new;
+  }
+  else
+  { struct node *t=head;
+    while(t->next!=NULL)
+    { t=t->next;
+    }
+    t->next=new;
+    new->prev=t;
+  }
+}
+void display()
+{ struct node *ptr=head;
+  while(ptr!=NULL)
+  { printf("%d\t",ptr->data);
+    ptr=ptr->next;
+  }
+}
+int main()
+{ int n,i,k;
+  printf("enter the number of nodes\n");
+  scanf("%d",&n);
+  for(i=0;i<n;++i)
+  { printf("enter the data\n");
+    scanf("%d",&k);
+    create(k);
+  }
+  display();
+  return 0;
+}
